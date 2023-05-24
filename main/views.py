17dbb8ec885183a38f2a165e6a9debd19b93faba
Rameshk Amir <rameshkamir@gmail.com>
2023-05-24 18:11:14 +0300

    Add some changes

--- main/views.py
+++ main/views.py
@@ -1,16 +1,36 @@
 from django.shortcuts import render
+import json
+from messages_home.models import Conversation
+from django.db.models import Count
 
-
-# Create your views here.
 def index(request):
-  data = {
-    'title': 'Главная страница',
-  }
-  return render(request, 'main/index.html', data)
+    # Получение данных из базы данных
+    conversations = Conversation.objects.all()
+    data = {
+        'labels': [],
+        'datasets': [
+            {'label': 'Активная', 'data': []},
+            {'label': 'Закрыта', 'data': []},
+            {'label': 'Отложена', 'data': []}
+        ]
+    }
+    
+    # Формирование данных для графика
+    for conversation in conversations:
+        created_at = conversation.created_at.date().strftime('%Y-%m-%d')
+        data['labels'].append(created_at)
+        if conversation.status == 'Активная':
+            data['datasets'][0]['data'].append(created_at)
+        elif conversation.status == 'Закрыта':
+            data['datasets'][1]['data'].append(created_at)
+        elif conversation.status == 'Отложена':
+            data['datasets'][2]['data'].append(created_at)
 
+    print(data)
+    return render(request, "main/index.html", {"data": json.dumps(data)})
 
 def settings(request):
   data = {
-    'title': 'Настройки',
+    "title": "Настройки",
   }
-  return render(request, 'main/settings.html', data)
+  return render(request, "main/settings.html", data)
--- users/views.py
+++ users/views.py
@@ -1,4 +1,34 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from messages_home.models import Conversation, Message, Attachment
+from urllib.parse import unquote_plus
+from django.db.models import Q
 
 def users(request):
-    return render(request, 'users/users.html')
+    users = User.objects.all()
+    conversations = Conversation.objects.all()
+    
+
+    if request.method == 'GET':
+        user_name = request.GET.get('username')
+        user_access = request.GET.get('useraccess')
+
+        if user_name:
+            user_name = unquote_plus(user_name)
+            user_name_parts = user_name.split()
+            if len(user_name_parts) >= 2:
+                user_name_filter = Q(first_name__icontains=user_name_parts[0]) | Q(last_name__icontains=user_name_parts[1])
+            else:
+                user_name_filter = Q(first_name__icontains=user_name) | Q(
+                last_name__icontains=user_name)
+            users = users.filter(user_name_filter)
+        if user_access:
+            user_access = unquote_plus(user_access)
+            user_access_filter = Q(groups__name__icontains=user_access)
+            users = users.filter(user_access_filter)
+    context = {
+        'users': users,
+        'conversations': conversations,
+    }
+
+    return render(request, 'users/users.html', context=context)
